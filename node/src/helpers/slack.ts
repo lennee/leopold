a69de515b0cb3877f13055ed4ee9208852ded846
Tristan Smith <tas09914@gmail.co>
2021-04-17 22:20:59 -0400

    refactored python directory structure

--- node/src/helpers/slack.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const sendMessage = (message: string, channelName:string = "SLACK_GENERAL") => axios.get(
-    `https://slack.com/api/chat.postMessage?channel=${process.env[channelName]}&text=${encodeURIComponent(message)}`, {
-    headers: {
-      "Authorization": `Bearer ${process.env.SLACK_BOT_TOKEN}`,
-    },
-  });
