72134a85fbb14f067c4507387eb859ed060547c7
Tristan Smith <39088742+lennee@users.noreply.github.com>
2022-03-30 19:37:43 -0400

    updated api organizing (#10)
    * updated api organizing
    
    * updated api/service model
    
    * added newlines to end of files
    
    Co-authored-by: Tristan Smith <tas09914@gmail.co>

---
+++ src/routers/leopold.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import * as asana from '../services/asana'
+import * as slack from '../services/slack'
+import * as leopold from '../services/leopold'
+
+const leopoldRouter = Router()
+
+leopoldRouter.get('/sendToDoList', (req, res) =>
+    asana.fetchTodo()
+      .then((data) => slack.sendMessage(leopold.formatToDoList(data), 'SLACK_TESTING'))
+      .then(() => res.sendStatus(200))
+      .catch((e) => res.status(500).send(e.message)));
+
+export = leopoldRouter
