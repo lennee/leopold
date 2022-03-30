5bff457474ceff164cc70555aa5a9a9deb649d8d
Tristan Smith <tas09914@gmail.co>
2022-03-30 19:34:30 -0400

    updated api organizing

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 
-import * as asana from "./helpers/asana";
-import { sendMessage } from "./helpers/slack";
-import { formatToDoList } from "./helpers/messages"
+import asana from "./routers/asana";
+import leopold from "./routers/leopold";
+import slack from "./routers/slack";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,25 +11,10 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/api/todoList', (req, res) =>
-  asana.fetchTodo()
-    .then(data => res.status(200).json(data))
-    .catch(() => res.sendStatus(500)));
-
-app.post('/api/sendMessage', (req, res) =>
-  sendMessage(req.body.message, 'SLACK_TESTING')
-    .then((s) => {
-      console.log(s.request._header);
-      res.sendStatus(200);
-    })
-    .catch((e) => console.log(`There was an error when sending the message to slack.\nError: ${e.message}`)));
-
-app.get('/api/sendToDoList', (req, res) =>
-    asana.fetchTodo()
-      .then((data) => sendMessage(formatToDoList(data), 'SLACK_TESTING'))
-      .then(() => res.sendStatus(200))
-      .catch((e) => res.status(500).send(e.message)));
-
+// Register Service Routers
+app.use('/api/asana', asana)
+app.use('/api/slack', slack)
+app.use('/api/leopold', leopold)
 
 const PORT = Number(process.env.PORT) || 3000;
 
