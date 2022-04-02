2f072fb174bb57ec6a0ed1f111034aae9507b78a
Tristan Smith <tristan.an.smith@gmail.com>
2022-04-02 19:49:23 -0400

    updated calendar to use promises

--- src/services/google_calendar.ts
+++ src/services/google_calendar.ts
@@ -3,20 +3,18 @@ import { calendar_v3, google } from 'googleapis'
 import { OAuth2Client } from 'google-auth-library'
 import Schema$Event = calendar_v3.Schema$Event
 
-export const listEvents = async (calendarId: string): Promise<Schema$Event[]> => new Promise(async (resolve, reject) => {
+export const listEvents = async (calendarId: string): Promise<Schema$Event[]> => {
 
   const auth: OAuth2Client = await getOAuthClient(Scopes.READ_CALENDAR);
 
   const calendar = google.calendar({version: 'v3', auth});
 
-  calendar.events.list({
+  return calendar.events.list({
     calendarId,
     timeMin: (new Date()).toISOString(),
     maxResults: 10,
     singleEvents: true,
     orderBy: 'startTime',
-  }, (err, data) => {
-    if (err) reject(err)
-    resolve(data.data.items)
-  });
-})
+  })
+    .then(data => Promise.resolve(data.data.items))
+}
