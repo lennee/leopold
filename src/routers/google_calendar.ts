dd2d626dc5d7ba0fabbda099a3f3f478da44a44a
Tristan Smith <tristan.an.smith@gmail.com>
2022-04-02 20:45:15 -0400

    added 'createEvent' method & endpoint to google calenar service

--- src/routers/google_calendar.ts
+++ src/routers/google_calendar.ts
@@ -1,4 +1,4 @@
-import * as google_calendar from '../services/google_calendar'
+import google_calendar from '../services/google_calendar'
 import { Router } from 'express'
 
 const googleCalendarRouter = Router()
@@ -6,6 +6,11 @@ const googleCalendarRouter = Router()
 googleCalendarRouter.get('/listEvents', (req, res) =>
   google_calendar.listEvents(req.body.calendarId)
     .then(data => res.status(200).json(data))
+    .catch(() => res.sendStatus(500)));
+
+googleCalendarRouter.get('/createEvent', (req, res) =>
+  google_calendar.createEvent(req.body.calendarId, req.body.event)
+    .then(() => res.status(200).send("Successfully Created Event"))
     .catch(() => res.sendStatus(500)));
 
 export default googleCalendarRouter
--- src/services/google_calendar.ts
+++ src/services/google_calendar.ts
@@ -3,7 +3,7 @@ import { calendar_v3, google } from 'googleapis'
 import { OAuth2Client } from 'google-auth-library'
 import Schema$Event = calendar_v3.Schema$Event
 
-export const listEvents = async (calendarId: string): Promise<Schema$Event[]> => {
+const listEvents = async (calendarId: string): Promise<Schema$Event[]> => {
 
   const auth: OAuth2Client = await getOAuthClient(Scopes.READ_CALENDAR);
 
@@ -17,4 +17,21 @@ export const listEvents = async (calendarId: string): Promise<Schema$Event[]> =>
     orderBy: 'startTime',
   })
     .then(data => Promise.resolve(data.data.items))
+}
+
+const createEvent = async (calendarId: string, event: Schema$Event) => {
+
+  const auth: OAuth2Client = await getOAuthClient(Scopes.FULL_CALENDAR);
+
+  const calendar = google.calendar({version: 'v3', auth});
+
+  return calendar.events.insert({
+    calendarId,
+    requestBody: event,
+  })
+}
+
+export default {
+  listEvents,
+  createEvent
 }
